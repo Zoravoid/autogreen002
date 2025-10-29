5483f83d1cec9508e772a5c4fd781c233653697e
Cynder <cynder877@gmail.com>
2025-10-29 18:27:30 +0100

    Links on sidebar works, preparations for header, looks scuffed currently

--- frontend/autogreen/app/_components/Navbar.tsx
+++ frontend/autogreen/app/_components/Navbar.tsx
@@ -3,10 +3,10 @@ import "./navbar.css";
 
 const Navbar = () => {
     return <div className="sidenav">
-        <a href="#">CameraFeed</a>
-        <a href="#">EnergyInfo</a>
-        <a href="#">PlantState</a>
-        <a href="#">SensorData</a>
+        <a href="/CameraFeed">CameraFeed</a>
+        <a href="/EnergyInfo">EnergyInfo</a>
+        <a href="/PlantState">PlantState</a>
+        <a href="/SensorData">SensorData</a>
     </div>;
 };
 
--- frontend/autogreen/app/layout.tsx
+++ frontend/autogreen/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from './_components/Navbar';
+import Navbar from "./_components/navbar";
+import Header from "./_components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="min-h-screen">
+          <Header />
           <Navbar />
           <main className="container mx-auto px-4 py-8"> {children}</main>
          
