a3043e2b2152557477adbe0ab4bcdcb5eee271d9
Your Name <you@example.com>
2025-11-14 13:04:05 +0100

    added logo in the corner of the ui and fixed the issue of graphs behind the sidebar

--- frontend/autogreen/app/layout.tsx
+++ frontend/autogreen/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./_components/_navigation/navbar";
 import Header from "./_components/_navigation/header";
+import Logo from "./_components/_navigation/logo";
 
 
 export const metadata: Metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({
         <div className="min-h-screen">
           <Header />
           <Navbar />
+          <Logo />
           <main className="container mx-auto px-4 py-8"> {children}</main>
          
 
