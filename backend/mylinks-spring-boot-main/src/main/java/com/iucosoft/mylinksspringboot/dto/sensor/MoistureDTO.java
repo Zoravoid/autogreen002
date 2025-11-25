4ccbe9804a83f200a60cf6e214a04b0f57b7914c
Zoravoid <florianalex070203@icloud.com>
2025-11-25 22:18:46 +0100

    working

--- backend/mylinks-spring-boot-main/src/main/java/com/iucosoft/mylinksspringboot/dto/sensor/MoistureDTO.java
+++ backend/mylinks-spring-boot-main/src/main/java/com/iucosoft/mylinksspringboot/dto/sensor/MoistureDTO.java
@@ -1,15 +1,19 @@
 package com.iucosoft.mylinksspringboot.dto.sensor;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import java.util.Date;
 
 @Data
 public class MoistureDTO {
 
+    @JsonProperty("device_id")
     private Long deviceId;
 
+    @JsonProperty("moisture_val")
     private Float moisture_val;
 
+    @JsonProperty("time_stamp")
     private Date time_stamp;
 
 }
