e86a349444f226de41f0e907a206b11c55fc1e19
Cynder <cynder877@gmail.com>
2025-12-11 19:25:50 +0100

    added placeholder graph for energy info and added the home page description and image

--- frontend/autogreen/app/EnergyInfo/page.tsx
+++ frontend/autogreen/app/EnergyInfo/page.tsx
@@ -1,7 +1,32 @@
+import TestChart2 from "../_components/_charts/testchart2";
+
 export default function EnergyInfo() {
   return (
+    <main>
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-        Hello From Energy Info Page!
+        This page is meant to show the amount of energy used and gained from the greenhouse, although currently the graph is simply a placeholder.
+
+      <div className="grid xl:grid-cols-1 lg:grid-cols-1 w-auto gap-10 max-w-[1400px]">
+          
+        <GridItem title="Energy Info">
+          <TestChart2 />
+        </GridItem>
+        
+      </div>
+
+    </div>
+    </main>
+    
+  );
+}
+
+function GridItem({ title, children}) {
+  return (
+    <div className="flex flex-col items-center justify-center p-4 border border-slate-900 
+    rounded-xl h-[400px]">
+      <h3 className="text-2xl font-semibold  mb-4" >{title}</h3>
+      {children} 
+
     </div>
   );
 }
--- frontend/autogreen/app/page.tsx
+++ frontend/autogreen/app/page.tsx
@@ -1,9 +1,32 @@
 import Image from "next/image";
+import "./home.css";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Welcome!
+    <div className="homePage">
+    <div className="items-center justify-items-center min-h-screen">
+       <div className="image">
+       <Image
+          src="/tomatoes.jpg"
+          alt="Tomato Picture"
+          width={400}
+          height={400}
+        />
+        </div>
+        <div className="text">
+          This systems was made for a student project called Automated 
+          Greenhouse. The project aims to design and implement a full-scale
+          automated greenhouse that integrates mechanical, mechatronic, 
+          electronic and software systems into a cohesive, intelligent 
+          agricultural environment. The project is being made together with
+          Szelgaard ApS, which is a company that focuses on developing future
+          technologies within the green sector. The system will employ an 
+          array of sensors, robotic mechanisms, and AI functionalities. The 
+          project should be approached mostly as a ptototype or proof of 
+          concept for what could become a fully automated greenhouse with 
+          minimal human supervision.
+        </div>
+    </div>
     </div>
   );
 }
