7c168b09eee09ca16024f3771af676ce37cf3ca6
Zoravoid <florianalex070203@icloud.com>
2025-12-14 17:29:51 +0100

    Merge branch 'main' into raspberri-pi

--- backend/mylinks-spring-boot-main/target/generated-sources/annotations/com/iucosoft/mylinksspringboot/mappers/UserMapperImpl.java
+++ backend/mylinks-spring-boot-main/target/generated-sources/annotations/com/iucosoft/mylinksspringboot/mappers/UserMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-29T02:32:08+0100",
+    date = "2025-11-30T18:57:18+0100",
     comments = "version: 1.5.4.Final, compiler: javac, environment: Java 1.8.0_472 (Private Build)"
 )
 @Component
